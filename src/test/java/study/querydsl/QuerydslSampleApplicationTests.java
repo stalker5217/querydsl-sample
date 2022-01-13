54e1c7cc9ed5609a6ea1f4fbacea8ac714be8f6a
Song <stalker5217@gmail.com>
2022-01-13 22:59:50 +0900

    동적 쿼리와 성능 최적화 조회

--- src/test/java/study/querydsl/QuerydslSampleApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslSampleApplicationTests.java
@@ -1,7 +1,11 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManager;
 
 @SpringBootTest
 class QuerydslSampleApplicationTests {
@@ -9,5 +13,4 @@ class QuerydslSampleApplicationTests {
     @Test
     void contextLoads() {
     }
-
 }
